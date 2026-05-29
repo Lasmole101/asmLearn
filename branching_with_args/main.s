2b982a906629b6cf73add4b51ee8eb630ea38b2e
Lasmole101 <lasmole101@gmail.com>
2026-05-29 23:22:20 +0100

    experiment with call and ret

--- branching_with_args/main.s
+++ branching_with_args/main.s
@@ -5,23 +5,26 @@
 .section .text
 
 _start:
-	mov $1,	%rax	#write syscall
-	mov $1, %rdi	#stdout, where to write to
-	mov $msgStart, %rsi	#string
-	mov $lenStart, %rdx	#string size
-	syscall
 
-  call _func1
+  mov $msgStart, %r8
+  mov $lenStart, %r9
+  call _print
+
+  mov $msgFunc, %r8
+  mov $lenFunc, %r9
+  call _print
 
 	mov $60, %rax	#exit syscall
 	mov $0, %rdi	#exit code
 	syscall
 
-_func1:
+#Print function that takes two args, The string to print and its length 
+#It expects the string id to be in r8 and the length to be in r9
+_print:
 	mov $1,	%rax	#write syscall
 	mov $1, %rdi	#stdout, where to write to
-	mov $msgFunc, %rsi	#string
-	mov $lenFunc, %rdx	#string size
+	mov %r8, %rsi	#string
+	mov %r9, %rdx	#string size
 	syscall
   ret
 
